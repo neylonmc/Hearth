2352f34c6307e4514f2826e6fde8301c920b8fc1
eaclumpkens <eaclumpkens@gmail.com>
2021-01-23 11:58:28 -0500

    mobile/desktop backgrounds

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {
   Route,
   Switch
 } from "react-router-dom";
+import { Container } from 'rsuite';
 import Login from "./pages/Login";
 import Select from "./pages/Select";
 import Dashboard from "./pages/Dashboard";
@@ -17,29 +18,31 @@ function App() {
   return (
     <Router>
       <div className="App">
-          <Switch>
-              <Route exact path={["/", "/login"]}>
-                <Login />
-              </Route>
-              <Route exact path="/select">
-                <Select />
-              </Route>
-              <Route exact path="/dashboard">
-                <Dashboard />
-              </Route>
-              <Route exact path="/profile">
-                <Profile />
-              </Route>
-              <Route exact path="/topic">
-                <Topic />
-              </Route>
-              <Route exact path="/settings">
-                <Settings />
-              </Route>
-              <Route path="*">
-                <NoMatch />
-              </Route>
-          </Switch>
+        <Container>
+            <Switch>
+                <Route exact path={["/", "/login"]}>
+                  <Login />
+                </Route>
+                <Route exact path="/select">
+                  <Select />
+                </Route>
+                <Route exact path="/dashboard">
+                  <Dashboard />
+                </Route>
+                <Route exact path="/profile">
+                  <Profile />
+                </Route>
+                <Route exact path="/topic">
+                  <Topic />
+                </Route>
+                <Route exact path="/settings">
+                  <Settings />
+                </Route>
+                <Route path="*">
+                  <NoMatch />
+                </Route>
+            </Switch>
+          </Container>
       </div>
     </Router>
   );
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import "./assets/css/style.css";
 
 ReactDOM.render(
   <React.StrictMode>
