2cdcbc24d2405be549f4659e430bcb811bd599aa
eaclumpkens <eaclumpkens@gmail.com>
2021-01-23 11:13:50 -0500

    added page routes and base files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from "react-router-dom";
+import Login from "./pages/Login";
+import Select from "./pages/Select";
+import Dashboard from "./pages/Dashboard";
+import Profile from "./pages/Profile";
+import Topic from "./pages/Topic";
+import Settings from "./pages/Settings";
+import NoMatch from "./pages/NoMatch";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+          <Switch>
+              <Route exact path={["/", "/login"]}>
+                <Login />
+              </Route>
+              <Route exact path="/select">
+                <Select />
+              </Route>
+              <Route exact path="/dashboard">
+                <Dashboard />
+              </Route>
+              <Route exact path="/profile">
+                <Profile />
+              </Route>
+              <Route exact path="/topic">
+                <Topic />
+              </Route>
+              <Route exact path="/settings">
+                <Settings />
+              </Route>
+              <Route path="*">
+                <NoMatch />
+              </Route>
+          </Switch>
+      </div>
+    </Router>
   );
 }
 
