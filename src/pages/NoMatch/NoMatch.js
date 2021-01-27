a8a0dd441d639ce1d81e23b893344a5a986b12b5
eaclumpkens <eaclumpkens@gmail.com>
2021-01-27 18:01:01 -0500

    fixed css heirarchy

--- src/pages/NoMatch/NoMatch.js
+++ src/pages/NoMatch/NoMatch.js
@@ -6,9 +6,8 @@ function NoMatch() {
     return(
         <div className="no-match-container">
             <img className="no-match-image" src="https://media.giphy.com/media/6uGhT1O4sxpi8/source.gif" alt="confused pulp fiction gif"/>
-            <h1 className="no-match-title">whoops</h1>
-            <h2 id="no-match-body">we can't seem to find the page you're looking for</h2>
-            <h2 id="no-match-body"><strong> try again </strong></h2>
+            <h1 className="no-match-title">404: No Results</h1>
+            <h2 id="no-match-body">we can't find the page you're looking for</h2>
         </div>
     )
 
