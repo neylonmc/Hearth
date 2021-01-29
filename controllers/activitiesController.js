23f636a6c53bfafa63de04d9fd8a05f1c5a598d9
jaguar6105 <gilljoseph603@gmail.com>
2021-01-28 19:38:45 -0500

    change .find from category to type and add proxy

--- controllers/activitiesController.js
+++ controllers/activitiesController.js
@@ -17,14 +17,14 @@ module.exports = {
   },
   findByType: function (req, res) {
     db.Activity
-      .find({category: req.params.type})
+      .find({type: req.params.type})
       .sort({ date: -1 })
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
   findByHighest: function (req, res) {
     db.Activity
-      .find({category: req.params.type})
+      .find({type: req.params.type})
       .sort({ averageRating: -1})
       .limit(1)
       .then(dbModel => res.json(dbModel))
