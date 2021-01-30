0fdb691d4d03d92df07231fd23a707125343687a
jaguar6105 <gilljoseph603@gmail.com>
2021-01-30 11:31:28 -0500

    form in createNew now puts input into state

--- src/components/CreateNew/CreateNew.js
+++ src/components/CreateNew/CreateNew.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {
   Form,
   FormGroup,
@@ -15,8 +15,61 @@ import {
 import "rsuite/lib/styles/index.less";
 import "./CreateNewStyle.css";
 import selections from "../../utils/Genre";
+import API from "../../utils/API";
+
 
 const CreateNew = () => {
+  const [developerState, setDeveloperState] = useState({});
+  const [formObject, setFormObject] = useState({})
+
+  useEffect(() => {
+  }, [])
+
+
+  function createPost() {
+  }
+
+  /*function handleInputChange(event) {
+    console.log(event.target);
+    const { name, value } = event.target;
+    setFormObject({...formObject, [name]: value})
+  };*/
+
+
+  function handleFormSubmit(event) {
+    event.preventDefault();
+    console.log(formObject);
+  }
+
+  function handleFormCancel(event) {
+    event.preventDefault();
+    setFormObject({ })
+  }
+
+
+  //need these over handleInputChange because event.target is undefined for non-input forms
+  function handleInputChangeRadio(event) {
+    setFormObject({ ...formObject, type: event })
+  };
+
+  function handleInputChangeTitle(event) {
+    setFormObject({ ...formObject, title: event })
+  };
+
+  function handleInputChangeRating(event) {
+    setFormObject({ ...formObject, rating: event })
+  };
+
+  function handleInputChangeGenre(event) {
+    setFormObject({ ...formObject, category: event })
+  };
+
+  function handleInputChangeReview(event) {
+    setFormObject({ ...formObject, review: event })
+  };
+
+
+
   return (
     <div className="flex">
       <h1 className="title-item" style={{ backgroundColor: "#d9a51f" }}>
@@ -26,37 +79,43 @@ const CreateNew = () => {
       <Form fluid className="form-style">
         <FormGroup controlId="radioList">
           <FormControlLabel>What Would You Like To Review?</FormControlLabel>
-          <RadioGroup name="radioList" inline>
-            <Radio value="A">Movie</Radio>
-            <Radio value="B">Tv</Radio>
-            <Radio value="C">Book</Radio>
-            <Radio value="D">Music</Radio>
+          <RadioGroup
+            onChange={handleInputChangeRadio}
+            name="radioList" inline>
+            <Radio value="Movie">Movie</Radio>
+            <Radio value="TV">Tv</Radio>
+            <Radio value="Book">Book</Radio>
+            <Radio value="Music">Music</Radio>
           </RadioGroup>
         </FormGroup>
         <FormGroup>
           <FormControlLabel>Please Enter the Title</FormControlLabel>
-          <FormControl name="title" type="input" className="form-title" />
+          <FormControl onChange={handleInputChangeTitle}
+            name="title" type="input" className="form-title" />
           <FormHelpText>Required</FormHelpText>
         </FormGroup>
         <FormGroup>
           <FormControlLabel>Please Select You Rating</FormControlLabel>
-          <Rate defaultValue={2.5} allowHalf style={{ color: "#62130a" }} />
+          <Rate onChange={handleInputChangeRating} name="rating" defaultValue={2.5} allowHalf style={{ color: "#62130a" }} />
         </FormGroup>
         <FormGroup>
           <FormControlLabel>Please Select a Genre</FormControlLabel>
           <SelectPicker
             data={selections}
             style={{ width: 224, marginBottom: "10px" }}
+            onChange={handleInputChangeGenre}
+            name="category"
           />
           <FormControlLabel>Leave A Review!</FormControlLabel>
-          <FormControl rows={3} name="textarea" componentClass="textarea" />
+          <FormControl onChange={handleInputChangeReview} rows={3} name="textarea" componentClass="textarea" />
         </FormGroup>
         <FormGroup>
           <ButtonToolbar>
-            <Button style={{ backgroundColor: "#62130a", color: "#d9a51f" }}>
+            <Button style={{ backgroundColor: "#62130a", color: "#d9a51f" }}
+              onClick={handleFormSubmit}>
               Submit
             </Button>
-            <Button appearance="default">Cancel</Button>
+            <Button appearance="default"  onClick={handleFormCancel}>Cancel</Button>
           </ButtonToolbar>
         </FormGroup>
       </Form>
