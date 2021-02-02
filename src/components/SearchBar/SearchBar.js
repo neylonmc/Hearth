4d038493b7d29c55bce90c13640c7bb2dfcc4a3d
eaclumpkens <eaclumpkens@gmail.com>
2021-02-02 15:42:35 -0500

    shuffle entertainment

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,27 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {
     InputGroup,
     Input,
+    InputPicker
 } from "rsuite";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import Entertainment from "../../utils/TopEntertainment";
 import "./SearchBar.css";
 
 function SearchBar() {
 
+    useEffect(() => {
+        console.log(Entertainment);
+    })
+
     return(
-
-        <div className="search-container">
-            <InputGroup inside className="search-group">
-                <Input className="search-input"/>
-                <InputGroup.Button className="search-button">
-                    <FontAwesomeIcon className="search-icon" icon={faSearch}/>
-                </InputGroup.Button>
-            </InputGroup>
-        </div>
-
+        <InputPicker 
+            className= "search-container"
+            data={ Entertainment } 
+            size="lg"
+            placeholder="search"
+        />
     )
-
 };
 
 export default SearchBar;
+
+/* <div className="search-container">
+<InputGroup inside className="search-group">
+    <Input 
+        className="search-input" 
+        onChange={ onChange } 
+        type="search" 
+        placeholder="search"
+    />
+    <InputGroup.Button className="search-button">
+        <FontAwesomeIcon className="search-icon" icon={faSearch}/>
+    </InputGroup.Button>
+</InputGroup>
+</div> */
--- src/utils/TopEntertainment.js
+++ src/utils/TopEntertainment.js
@@ -1,186 +1,233 @@
  const topEntertainment = [
      {
-         title: "The Catcher in the Rye",
-         topic: "books",
-         genre: "Drama",
-         img: "/images/topEntertainment/catcher-rye.jpg",
-         img_dev: "../../public/images/topEntertainment/catcher-rye.jpg"
+        title: "The Catcher in the Rye", 
+        label: "the catcher in the rye",
+        topic: "books",
+        genre: "Drama",
+        img: "/images/topEntertainment/catcher-rye.jpg"
      },{
         title: "Dune",
+        label: "dune",
         topic: "books",
         genre: "Action, Adventure, Sci-Fi",
         img: "/images/topEntertainment/dune.jpg"
     },{
         title: "The Haunting of Hill House",
+        label: "the haunting of hill house",
         topic: "books",
         genre: "Drama, Horror, Mystery, Thriller",
         img: "/images/topEntertainment/hill-house.jpg"
     },{
         title: "The Hobbit",
+        label: "the hobbit",
         topic: "books",
         genre: ["Adventure", "Fantasy"],
         img: "/images/topEntertainment/hobbit.jpg"
     },{
         title: "The Hunger Games",
+        label: "the hunger games",
         topic: "books",
         genre: ["Action", "Adventure", "Sci-Fi", "Thriller"],
         img: "/images/topEntertainment/hunger-games.jpg"
     },{
         title: "Titanic",
+        label: "titanic",
         topic: "movies",
         genre: ["Drama", "Romance"],
         img: "/images/topEntertainment/titanic.jpg"
     },{
         title: "Star Wars",
+        label: "star wars",
         topic: "movies",
         genre: ["Action", "Adventure", "Fantasy", "Sci-Fi"],
         img: "/images/topEntertainment/star-wars.jpg"
     },{
         title: "Gone with the Wind",
+        label: "gone with the wind",
         topic: "movies",
         genre: ["Drama", "History", "Romance", "War"],
         img: "/images/topEntertainment/gone-wind.jpg"
     },{
         title: "Dumb and Dumber",
+        label: "dumb and dumber",
         topic: "movies",
         genre: "Comedy",
         img: "/images/topEntertainment/dumb-dumber.jpg"
     },{
         title: "Spirited Away",
+        label: "spirited away",
         topic: "movies",
         genre: ["Animation", "Adventure", "Family", "Fantasy", "Mystery"],
         img: "/images/topEntertainment/spirited-away.jpg"
     },{
         title: "The Dark Knight",
+        label: "the dark knight",
         topic: "movies",
         genre: ["Action", "Crime", "Drama", "Thriller"],
         img: "/images/topEntertainment/dark-knight.jpg"
     },{
         title: "Soul",
+        label: "soul",
         topic: "movies",
         genre: ["Animation", "Adventure", "Comedy", "Family", "Fantasy", "music"],
         img: "/images/topEntertainment/soul.jpg"
     },{
         title: "Game of Thrones",
+        label: "game of thrones",
         topic: "television",
         genre: ["Action", "Drama", "Fantasy", "Romance"],
         img: "/images/topEntertainment/game-thrones.jpg"
     },{
         title: "The Mandalorian",
+        label: "the mandalorian",
         topic: "television",
         genre: ["Action", "Adventure", "Sci-Fi"],
         img: "/images/topEntertainment/mandalorian.jpg"
     },{
         title: "The Crown",
+        label: "the crown",
         topic: "television",
         genre: ["Biography", "Drama", "History"],
         img: "/images/topEntertainment/crown.jpg"
     },{
         title: "Attack on Titan",
+        label: "attack on titan",
         topic: "television",
         genre: ["Animation", "Action", "Adventure", "Drama", "Fantasy", "Horror"],
         img: "/images/topEntertainment/attack-titan.jpg"
     },{
         title: "Spongebob Squarepants",
+        label: "spongebob squarepants",
         topic: "television",
         genre: ["Animation", "Comedy", "Family", "Fantasy"],
         img: "/images/topEntertainment/spongebob.jpg"
     },{
         title: "Mindhunter",
+        label: "mindhunter",
         topic: "television",
         genre: ["Crime", "Drama", "Thriller"],
         img: "/images/topEntertainment/mindhunter.jpg"
     },{
         title: "Planet Earth",
+        label: "planet earth",
         topic: "television",
         genre: "Documentary",
         img: "/images/topEntertainment/planet-earth.jpg"
     },{
         title: "Pulp Fiction",
+        label: "pulp fiction",
         topic: "movies",
         genre: ["Crime", "Drama"],
         img: "/images/topEntertainment/pulp-fiction.jpg"
     },{
         title: "Euphoria",
+        label: "euphoria",
         topic: "television",
         genre: ["Drama", "LGTBQ"],
         img: "/images/topEntertainment/euphoria.jpg"
     },{
         title: "Americanah",
+        label: "americanah",
         topic: "books",
         genre: ["Drama", "Romance"],
         img: "/images/topEntertainment/americanah.jpg"
     },{
         title: "House",
+        label: "house",
         topic: "television",
         genre: ["Drama", "Mystery"],
         img: "/images/topEntertainment/house.jpg"
     },{
         title: "Rick and Morty",
+        label: "rick and morty",
         topic: "television",
         genre: ["Animation", "Adventure", "Comedy", "Sci-Fi"],
         img: "/images/topEntertainment/rick-morty.jpg"
     },{
         title: "Lady Gaga",
+        label: "lady gaga",
         topic: "music",
         genre: "Pop",
         img: "/images/topEntertainment/lady-gaga.jpg"
     },{
         title: "Drake",
+        label: "drake",
         topic: "music",
         genre: ["Rap", "R&B"],
         img: "/images/topEntertainment/drake.jpg"
     },{
         title: "Tim McGraw",
+        label: "tim mcgraw",
         topic: "music",
         genre: "Country",
         img: "/images/topEntertainment/tim-mcgraw.jpg"
     },{
         title: "Billie Eilish",
+        label: "billie eilish",
         topic: "music",
         genre: "Pop",
         img: "/images/topEntertainment/billie-eilish.jpg"
     },{
         title: "Fleetwood Mac",
+        label: "fleetwood mac",
         topic: "music",
         genre: "Classic Rock",
         img: "/images/topEntertainment/fleetwood-mac.jpg"
     },{
         title: "The Smiths",
+        label: "the smiths",
         topic: "music",
         genre: ["Rock", "Britpop"],
         img: "/images/topEntertainment/the-smiths.jpg"
     },{
         title: "H.E.R.",
+        label: "h.e.r.",
         topic: "music",
         genre: ["R&B", "Soul"],
         img: "/images/topEntertainment/h-e-r.jpg"
     },{
         title: "Zedd",
+        label: "zedd",
         topic: "music",
         genre: ["Dance", "Electronic"],
         img: "/images/topEntertainment/zedd.jpg"
     },{
         title: "Black Sabbath",
+        label: "black sabbath",
         topic: "music",
         genre: ["Classic Metal", "Metal"],
         img: "/images/topEntertainment/black-sabbath.jpg"
     },{
         title: "Nirvana",
+        label: "nirvana",
         topic: "music",
         genre: "Punk Rock",
         img: "/images/topEntertainment/nirvana.jpg"
     },{
         title: "Jojo Siwa",
+        label: "jojo siwa",
         topic: "music",
         genre: ["Pop", "Family"],
         img: "/images/topEntertainment/jojo-siwa.jpg"
     },{
         title: "One Direction",
+        label: "one direction",
         topic: "music",
         genre: ["Rock", "Britpop"],
         img: "/images/topEntertainment/one-direction.jpg"
     }
  ];
 
- export default topEntertainment;
+ function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+    while (0 !== currentIndex) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+    return array;
+  }
+
+ export default shuffle(topEntertainment);
