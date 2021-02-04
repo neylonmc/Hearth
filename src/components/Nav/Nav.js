986e3ca90c93904a962ddcc7accac3d3830ada09
Meaghan Neylon <mneylon@richmond.edu>
2021-02-04 12:52:57 -0500

    Fixed navbar issues.

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -2,61 +2,9 @@ import React, { Component } from "react";
 import "./Nav.css";
 import { MenuItems } from "./MenuItem";
 import SearchBar from "../SearchBar/SearchBar";
-import Header from "../Header/Header";
 
 export class Navbar extends Component {
   state = { clicked: false };
-
-
-// const Header = ({ onSelect, activeKey }) => {
-//   return (
-//     <Navbar
-//       className="navContainer"
-//       style={{ width: "100%", backgroundColor: "transparent" }}
-//     >
-//       <Navbar.Header>
-//         <Link className="title" to="/">
-//           hearth
-//         </Link>
-//       </Navbar.Header>
-//       <Navbar.Body>
-//         <Nav onSelect={onSelect} activeKey={activeKey} pullRight>
-//           <Nav.Item eventKey="1">
-//             <Link id="nav-links" to="/dashboard">
-//               dashboard
-//             </Link>
-//           </Nav.Item>
-//           <Nav.Item eventKey="2">
-//             <Link id="nav-links" to="/profile">
-//               profile
-//             </Link>
-//           </Nav.Item>
-//           <Nav.Item eventKey="3">
-//             <Link id="nav-links" to="/topics">
-//               topics
-//             </Link>
-//           </Nav.Item>
-//           <Nav.Item eventKey="4">
-//             <Link id="nav-links" to="/post">
-//               new post
-//             </Link>
-//           </Nav.Item>
-//           <Nav.Item eventKey="5">
-//             <Link id="nav-links" to="/settings">
-//               settings
-//             </Link>
-//           </Nav.Item>
-//           <Nav.Item eventKey="6">
-//             <Link id="nav-links" to="/login">
-//               log out
-//             </Link>
-//           </Nav.Item>
-//         </Nav>
-//         <SearchBar />
-//       </Navbar.Body>
-//     </Navbar>
-//   );
-// };
 
   handleClick = () => {
     this.setState({ clicked: !this.state.clicked });
@@ -67,7 +15,6 @@ export class Navbar extends Component {
       <nav className="navbarItems">
         <h1 className="navbar-logo">hearth</h1>
         <div className="menu-icon" onClick={this.handleClick} pullRight>
-          <Header />
           <i className={this.state.clicked ? "fas fa-times" : "fas fa-bars"} />
         </div>
 
