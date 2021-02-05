6ea3f9c98091bcb4adef231a8266b59132a06797
jaguar6105 <gilljoseph603@gmail.com>
2021-02-04 20:16:15 -0500

    browse topic page now fills with topics from database

--- src/pages/Browse-Topics/Browse.js
+++ src/pages/Browse-Topics/Browse.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {
   Switch,
   Route,
@@ -6,7 +6,7 @@ import {
   useParams,
   useRouteMatch
 } from "react-router-dom";
-import { 
+import {
   ButtonGroup,
   Row,
   Col,
@@ -17,6 +17,7 @@ import "rsuite/lib/styles/index.less";
 import "./Browse.css";
 import allTopics from "../../utils/Topics";
 import Entertainment from "../../utils/TopEntertainment";
+import API from "../../utils/API";
 
 export default function Topics() {
   let { path, url } = useRouteMatch();
@@ -25,25 +26,25 @@ export default function Topics() {
     <div className="browse-container  animate__animated animate__fadeIn">
       <h3 className="browse-title">browse by topic</h3>
       <Row className="topic-links">
-        { allTopics.map(topic => {
-            return( 
-              <Col id="link-container" md={6} sm={12}>
-                <Link 
-                  id="topic-button"
-                  to={`${url}/${topic.topic}`}
-                >
-                  {topic.topic}
-                </Link>
-              </Col>
+        {allTopics.map(topic => {
+          return (
+            <Col id="link-container" md={6} sm={12}>
+              <Link
+                id="topic-button"
+                to={`${url}/${topic.api}`}
+              >
+                {topic.topic}
+              </Link>
+            </Col>
 
-            )
-          })
+          )
+        })
         }
       </Row>
-      
+
       <Switch>
         <Route exact path={path}>
-          <div className="empty-container"/>
+          <div className="empty-container" />
         </Route>
         <Route exact path={`${path}/:topic`}>
           <Topic />
@@ -57,37 +58,56 @@ function Topic() {
 
   let { topic } = useParams();
 
+  const [formObject, setFormObject] = useState([]);
+
+  useEffect(() => {
+    loadActivities(topic);
+  }, [topic])
+
+
+  function loadActivities(topic) {
+    API.getActivitiesByType(topic)
+      .then(res => {
+        setFormObject(res.data);
+      })
+      .catch(err => console.log(err));
+
+  }
+
+
+
+
   return (
-    <div className="topic-container  animate__animated animate__fadeIn">
+    <div className="topic-container  animate__animated animate__fadeIn" >
       <Panel
         className="topic-panel"
       >
         <ButtonGroup className="button-container">
-            { Entertainment.map(data => {
-              if (data.topic === topic) {
-                return(
-                  <Col 
-                    id="results-container"
-                    className="animate__animated animate__fadeIn"
-                    md={4} 
-                    sm={12}
-                  >
-                      <Button  
-                        id="results-button"
-                        href={ data.local_ext }
-                        bordered
-                      >
-                          <img 
-                              id= "topic-image"
-                              src= { data.img }
-                              alt= { data.title }
-                          />
-                      </Button>
-                  </Col>
+          {formObject.map(data => {
+            return (
+              <Col
+                id="results-container"
+                className="animate__animated animate__fadeIn"
+                md={4}
+                sm={12}
+              >
+                <p>{data.title}</p>
+                <Button
+                  id="results-button"
+                  href={data.local_ext}
+                  bordered
+                >
+                  <img
+                    id="topic-image"
+                    src={data.img}
+                    alt={data.title}
+                  />
+                </Button>
+              </Col>
 
-                );
-              } 
-            }) }
+            );
+
+          })}
         </ButtonGroup>
       </Panel>
     </div>
@@ -96,5 +116,5 @@ function Topic() {
 }
 
 
-  
+
 
