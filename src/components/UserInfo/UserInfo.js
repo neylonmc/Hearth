b619757032dcce232d7d3e398e3d4d2d8de53531
eaclumpkens <eaclumpkens@gmail.com>
2021-02-04 19:46:44 -0500

    userinfo placeholder

--- src/components/UserInfo/UserInfo.js
+++ src/components/UserInfo/UserInfo.js
@@ -1,10 +1,53 @@
 import React from "react";
+import {
+    Row,
+    Col
+} from "rsuite";
 import "./UserInfo.css"
+import Streaming from "../../components/Streaming/Streaming";
 
 function UserInfo() {
 
     return (
-        <p>hello world</p>
+        <div className="info-container">
+            <Row>
+                <Streaming />
+            </Row>
+            <Row>
+                <Col md={12}>
+                    <p>
+                        <strong 
+                            id="bold"
+                        > Joined : </strong>
+
+                        { "Jan-2021" } 
+                    </p>
+                    <p>
+                        <strong 
+                            id="bold"
+                        > Top Genres : </strong>
+                        
+                        { "Rock, Pop" }
+                    </p>
+                </Col>
+                <Col id="right-info" md={12}>
+                    <p>
+                        <strong 
+                            id="bold"
+                        > Following : </strong>
+
+                        { 0 } 
+                    </p>
+                    <p>
+                        <strong 
+                            id="bold"
+                        > Follower : </strong>
+
+                        { 0 } 
+                    </p>
+                </Col>
+            </Row>
+        </div>
     )
 
 }
