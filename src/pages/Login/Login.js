214d72698ece93d919ddf1b20393451ec1c6378f
Meaghan Neylon <mneylon@richmond.edu>
2021-02-08 22:02:05 -0500

    Made login page responsive.

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -3,33 +3,30 @@ import "./Login.css";
 import GoogleLogin from "../../components/GoogleAuth/GoogleLogin/GoogleLogin";
 import LoginLogo from "../../components/Logo/LoginLogo";
 import MediaGrid from "../../components/MediaGrid/MediaGrid";
-import {
-  Row,
-  Col
-} from "rsuite";
-
-
+import { Row, Col } from "rsuite";
 
 function Login() {
-
   return (
     <div className="login-container">
-        <Row>
-          <Col className="left-panel animate__animated animate__fadeIn" md={12}>
-            <LoginLogo/>
-            <h3 className="sub-head">
-              let your hobbies do the talking
-            </h3>
+      <Row>
+        <Col className="left-panel animate__animated animate__fadeIn" md={12}>
+          <LoginLogo />
+          <h3 className="sub-head">
+            let your hobbies{" "}
+            <br class="mobile-break" style={{ width: "2000px" }}></br> do the
+            talking
+          </h3>
+          <div className="login-btn">
             <GoogleLogin />
-          </Col>
+          </div>
+        </Col>
 
-          <Col className="right-panel animate__animated animate__fadeIn" md={12}>
-            <MediaGrid/>
-          </Col>
-        </Row>
+        <Col className="right-panel animate__animated animate__fadeIn" md={12}>
+          <MediaGrid />
+        </Col>
+      </Row>
     </div>
-  )
-
+  );
 }
 
 export default Login;
