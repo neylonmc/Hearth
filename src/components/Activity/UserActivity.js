795c0d23a7c90c8ef35fcaa68bc2702b55946216
eaclumpkens <eaclumpkens@gmail.com>
2021-02-10 23:22:07 -0500

    useact pause

--- src/components/Activity/UserActivity.js
+++ src/components/Activity/UserActivity.js
@@ -1,16 +1,38 @@
 import React, { useEffect, useState } from "react";
 import "./Activity.css"
+import { 
+    Row, 
+    Col,
+    Panel
+} from "rsuite";
 
 
 function UserActivity(props) {
 
+    const [revState, setRev ] = useState([])
 
-    var ratings = props.user.Ratings;
-    console.log(props.user);
-    console.log(ratings)
+    useEffect(() => {
+        var ratings = props.user.Ratings;
+        setRev(ratings);
+    }, [props.user.Ratings])
+
 
     return (
-        <p>hello world</p>
+        <div>
+            <p>hello world</p>
+            {/* <Row>
+                {revState.map(data => {
+                    return(
+                        <Col md={6} sm={12}>
+                            <Panel>
+                                <h2>{data.title || "title" }</h2>
+                                <p>{data.review || "body" }</p>
+                            </Panel>
+                        </Col>
+                    )
+                })}
+            </Row> */}
+        </div>
     )
 
 }
