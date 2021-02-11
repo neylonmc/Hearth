4e0bf7cdad82ebbd56920360e4fae1572a629652
eaclumpkens <eaclumpkens@gmail.com>
2021-02-11 15:33:46 -0500

    shortened reviews

--- src/components/Activity/UserActivity.js
+++ src/components/Activity/UserActivity.js
@@ -16,7 +16,7 @@ function UserActivity(props) {
         {
             topic: "Dune",
             title: "One of my Favorites",
-            review: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            review: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
             rating: 5,
             related_titles: ["The Mandalorian", "Star Wars"]
         },
@@ -30,7 +30,7 @@ function UserActivity(props) {
         {
             topic: "Ender's Game",
             title: "I had some issues with...",
-            review: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            review: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
             rating: 2,
             related_titles: ["The Mandalorian", "Star Wars", "Dune", "Star Trek", "The Clone Wars", "Rebels" ]
         }
