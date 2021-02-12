c719130fef6ba44ec2f4499b2d343de7de03e85f
Meaghan Neylon <mneylon@richmond.edu>
2021-02-11 23:08:08 -0500

    favicon

--- passport/connection.js
+++ passport/connection.js
@@ -1,19 +1,26 @@
 const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
 
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/hearthdb", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI ||  "mongodb://localhost:27017/hearthdb").then(
-  () => { 
-    /** ready to use. The `mongoose.connect()` promise resolves to undefined. */ 
-    console.log("Connected to Mongo");
-    
-},
-err => {
-     /** handle initial connection error */ 
-     console.log("error connecting to Mongo: ")
-     console.log(err);
-     
+mongoose
+  .connect(process.env.MONGODB_URI || "mongodb://localhost:27017/hearthdb")
+  .then(
+    () => {
+      /** ready to use. The `mongoose.connect()` promise resolves to undefined. */
+      console.log("Connected to Mongo");
+    },
+    (err) => {
+      /** handle initial connection error */
+      console.log("error connecting to Mongo: ");
+      console.log(err);
     }
-);
+  );
 
 module.exports = mongoose.connection;
